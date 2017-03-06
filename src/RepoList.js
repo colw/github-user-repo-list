e2db97c556e536ecee40bc89591d02ef18eb09d8
colw <colw@outlook.com>
2017-03-06 17:08:36 +0100

    Reduce file size by importing debounce directly

--- src/RepoList.js
+++ src/RepoList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { debounce } from 'lodash'
+import debounce from 'lodash/debounce';
 import { FetchComponent, LinkExt } from './components'
 
 const DELAY_MS = 300;
